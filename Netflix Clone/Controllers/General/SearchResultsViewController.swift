655477ee91e788261bdbdd2513c663db30fa8193
angel012912 <angel_012912@hotmail.com>
2023-06-22 00:46:31 -0600

    Search titles  preview working

--- Netflix Clone/Controllers/General/SearchResultsViewController.swift
+++ Netflix Clone/Controllers/General/SearchResultsViewController.swift
@@ -7,7 +7,12 @@
 
 import UIKit
 
+protocol SearchResultsViewControllerDelegate: AnyObject {
+    func SearchResultsViewControllerDidTapItem(_ viewModel: TitlePreviewViewModel)
+}
+
 class SearchResultsViewController: UIViewController {
+    public weak var delegate: SearchResultsViewControllerDelegate?
     // Se define el array de datos que se van a renderear y se inicializa vacio
     public var titles: [Title] = [Title]()
     // Se configura la coleccion de la vista que se usará
@@ -49,5 +54,19 @@ extension SearchResultsViewController: UICollectionViewDelegate, UICollectionVie
         let title = titles[indexPath.row]
         cell.configure(with: title.poster_path ?? "")
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        let title = titles[indexPath.row]
+        let titleName = title.original_title ?? ""
+        APICaller.shared.getMovie(with: titleName) { [weak self] result in
+            switch result {
+            case .success(let videoElement):
+                self?.delegate?.SearchResultsViewControllerDidTapItem(TitlePreviewViewModel(title: titleName, youtubeView: videoElement, titleOverView: title.overview ?? ""))
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
 }
